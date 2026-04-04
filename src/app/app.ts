1212b21428019f64cc873118eaba7bcbcc3b0d0e
Jeffrey <TheCrablander@gmail.com>
2026-04-04 14:00:41 -0500

    attempt to add google sign in for only development mode

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {environment} from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { isDevMode } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +14,18 @@ export class App {
   protected readonly title = signal('angular-experiments');
   protected somevar = 'TEST';
   protected envVar = environment.MY_ENV_VAR;
+  protected devMode = isDevMode();
+  private http = inject(HttpClient);
+
+  ngOnInit() {
+    if(isDevMode()){
+      const DEV_AUTH_CALLBACK_URL = 'http://localhost:5006/auth-callback';
+      (window as any).handleCredentialResponse = (response: any) => {
+        this.http.post(
+          DEV_AUTH_CALLBACK_URL, 
+          { credential: response.credential }
+        ).subscribe();
+      };
+    }
+  }
 }
